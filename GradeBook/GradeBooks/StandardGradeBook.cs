4a2bbada25be35616ec1e570ff0e8679242f2a22
Davin Jimenez <31362013+Djjimenez895@users.noreply.github.com>
2019-12-28 17:09:02 -0800

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,7 +5,7 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(String name) : base(name)
         {
